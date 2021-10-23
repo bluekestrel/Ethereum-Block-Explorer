05955f7302ec68b643f47d3f1d2007a919b64ca6
Alexis Williams <dev@localhost>
2021-10-22 21:56:54 -0400

    Client now polls server and server looks up new block information to send to client

--- client/src/Explorer.js
+++ client/src/Explorer.js
@@ -5,25 +5,50 @@ class Explorer extends React.Component {
         super(props);
         this.state = {
             data: null,
+            count: 0,
         }
     }
 
     componentDidMount() {
-        this.callBackendAPI()
-            .then(res => this.setState({ data: res.message }))
+        this.callBackendAPI("/")
+            .then((res) => {
+                this.setState(
+                    {
+                        data: res.message
+                    },
+                    () => {
+                        this.checkForNewBlock();
+                    }
+                );
+            })
             .catch(err => console.log(err));
     }
 
     // fetching the GET route from the Express server which matches the GET route from server.js
-    callBackendAPI = async () => {
-        const response = await fetch('http://localhost:3042/');
+    callBackendAPI = async (route) => {
+        const response = await fetch(`http://localhost:3042${route}`);
         const body = await response.json();
+        console.log(body);
 
         if (response.status !== 200) {
             throw Error(body.message) 
         }
         return body;
     };
+
+    checkForNewBlock() {
+        setInterval(() => {
+            this.callBackendAPI("/poll")
+            .then((res) => {
+                this.setState(
+                    {
+                        data: JSON.stringify(res.block)
+                    }
+                );
+            })
+            .catch(err => console.log(err));
+        }, 10000)
+    }
 
     render() {
         return (
--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const cors = require('cors');
 const express = require('express');
+const { ethers } = require('ethers');
 
+require('dotenv').config();
+
+const INFURA_API_KEY = process.env.INFURA_ENDPOINT;
 const app = express();
 const PORT = 3042;
+const provider = new ethers.providers.JsonRpcProvider(INFURA_API_KEY);
+
+let currentBlock;
 
 // localhost can have cross origin errors
 // depending on the browser you use!
@@ -10,8 +17,14 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({message: "Hello World!"});
+    res.json({ message: `Hello World!` });
 });
+
+app.get("/poll", async (req, res) => {
+    // use ethers to poll rinkeby for new blocks, return new block data to front-end
+    let newBlock = await provider.getBlock();
+    res.json({ block: newBlock });
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}!`);
